7618ec46793cade7f59fc4a84443f528b173a74c
qwas <git@qwas.fun>
2024-12-22 23:21:30 +0800

    chore: update next  to 14.2.21

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -53,9 +51,7 @@ export default function RootLayout({
     <html
       lang="zh"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
+        'text-black bg-white dark:text-white dark:bg-black'
       )}
     >
       <body className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto">
