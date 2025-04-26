56eda3feb88c8b5636273ee6932416c1c063e45e
qwas <git@qwas.fun>
2025-04-26 23:58:42 +0800

    feat: Add changelog
    - Add changelog to appFolders

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 /**
  * 在列表中展示的文件夹名称
  */
-export const appFolders = ['blog']
+export const appFolders = ['blog', 'changelog']
 
 export const siteTitle = 'Qwas'
 export const baseUrl = 'https://qwas.fun'
