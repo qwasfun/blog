d16b0f8ee5ecb84cc610cbaf09e388db744cf20d
qwas <git@qwas.fun>
2025-06-21 16:35:59 +0800

    fix: 删除无效引用

--- app/components/EnhancedImage.tsx
+++ app/components/EnhancedImage.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { Eye, EyeOff, Maximize2 } from 'lucide-react'
+import { Maximize2 } from 'lucide-react'
 import { FullscreenViewer } from './FullscreenViewer'
 import { useMobile } from '../../hooks/use-mobile'
 
